5468c38a224b5f3f92ef8bdfabe076b6f2a19afe
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-03-18 00:15:58 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,12 @@
-
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or my_dict == {}:
-        return None
-    biggest = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value is biggest:
-            return 
+def best_score(a_dictionary):
+    "returns a key with the biggest integer value"
+
+    biggest_value = 0
+    biggest_key = None
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if value > biggest_value:
+                biggest_value = value
+                biggest_key = key
+    return biggest_key
