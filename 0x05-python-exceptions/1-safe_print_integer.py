36fd9e02c96d0ec7899a77be2819619e623bb7a3
Abenezer Tekalign <abenezertekalign12@gmail.com>
2023-03-28 04:05:10 -0700

    TASKS

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     """Print an integer with "{:d}".format().
     Args:
@@ -13,4 +12,3 @@ def safe_print_integer(value):
         return (True)
     except (TypeError, ValueError):
         return (False)
-
