d637b4d2d9f1385e2963b563381009663e3219de
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-03-28 16:13:07 +0300

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,14 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Function based on Python bytecode"""
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too Far")
-            result += (a ** b) / i
-        except Exception:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
             result = b + a
             break
-    return result
+    return (result)
