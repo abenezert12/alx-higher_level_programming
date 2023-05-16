9337a03f9ee58f80a92e93992771a2efb5c7abc7
Abenezer Tekalign <abenezertekalign12@gmail.com>
2023-05-16 13:19:53 -0700

    TASKS

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,5 @@
--- list all the tables of database passed
--- as argument of mysql command
-SHOW TABLES;
+-- Script that deletes the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
