73b5a523e1422b56ef754ec6a34ff70770810c52
Abenezer Tekalign <abenezertekalign12@gmail.com>
2023-07-25 12:32:33 -0700

    TASKS

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
-/* Prints all characters of Star Wars movie */
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+const url = process.argv[2];
 
-request(url, (err, response, body) => {
-  if (err) console.log(err);
-  else {
-    const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], (err, response, body) => {
-        if (err) console.log(err);
-        else console.log(JSON.parse(body).name);
-      });
-    }
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
 });
